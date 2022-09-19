b4eafb3b607a5d9974718c9e0d82f21cfb4c4f37
Swati-Gupta20 <106022280+Swati-Gupta20@users.noreply.github.com>
2022-09-19 08:53:37 +0530

    Add files via upload

--- leapYear.py
+++ leapYear.py
@@ -1,14 +1,25 @@
 # Write a program to check whether an years is leap year or not.
 
 
+# yr=int(input("Enter the year:-"))
+# if yr%100==0:
+#     if yr%400==0:
+#           print("Entered year is leap year")
+#     else:
+#           print("Entered year is not a leap year")
+# else:
+#     if yr%4==0:
+#          print("Entered year is a leap year")  
+#     else:
+#         print("Entered year is not a leap year")
+
+
 yr=int(input("Enter the year:-"))
-if yr%100==0:
-    if yr%400==0:
-          print("Entered year is leap year")
-    else:
-          print("Entered year is not a leap year")
+if yr%4==0 and yr%100!=0:
+    print('leap year')
+elif yr%400==0 and yr%100==0:
+    print("leap year")
 else:
-    if yr%4==0:
-         print("Entered year is a leap year")  
-    else:
-        print("Entered year is not a leap year")
+    print("not a leap year")
+
+
